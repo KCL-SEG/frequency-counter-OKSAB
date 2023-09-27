1fc389d683ada8ffee94ab04542162b08a18badc
Osama Bugshan <osama.bugshan@kcl.ac.uk>
2023-09-27 15:33:18 +0100

    Add solution to frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,15 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    count = 0
+    for item in items:
+        item_to_string = str(item)
+
+        if item_to_string in frequencies:
+            count += 1
+            frequencies.update({str(item) : count})
+        else:
+            count = 1
+            frequencies.update({str(item) : count}) 
     return frequencies
+
