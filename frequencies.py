e265f16d5c3116b24bc52cbfc65410d593815b89
Osama Bugshan <osama.bugshan@kcl.ac.uk>
2023-09-28 12:14:51 +0100

    Add solution to frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,7 +4,6 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
-    count = 0
     for item in items: # Iterating throught the items
         item_to_string = str(item) # Convert items to string
 
